22ab8b3a79694a42ae1d65ffa90acedf2d97e1c0
Benjamin Ombeni <benjaminombeni3@gmail.com>
2020-01-03 20:58:28 -0800

    Updated

---
+++ dealer.py
@@ -0,0 +1,38 @@
+# By Benjamin
+
+class Dealer:
+    '''
+    INPUT: None
+    This is the dealer's class. 
+    '''
+    def __init__(self):
+        self.cards = [] # Place holder for the dealer's cards
+        self.hidden_card = 0 # Place holder for the hidden card
+
+    # ----------------------------
+    def hide_first_card(self):
+        '''
+        INPUT: None
+        Hides the first card given to the card.
+        '''
+        try:
+            self.hidden_card = self.cards[0]
+        except:
+            print('The dealer doe not have any card currenty.')
+            return None
+
+        self.cards[0] = 'X'
+
+    # --------------------------------
+    def reveal_hidden_card(self):
+        '''
+        INPUT: None
+        Reveals the dealer's hidden card if this one exists.
+        '''
+
+        # Checking if a card has been hidden previously
+        if self.hide_first_card != 0:
+            self.cards[0] = self.hidden_card
+        else:
+            print('The dealer has no hidden card currently.')
+            return None
